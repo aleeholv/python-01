be00a74edba0529e2a8895016f506a334bad52a9
Alessandra Lima <alessandra.lima24@escola.pr.gov.br>
2024-03-27 10:50:49 -0300

    subir aula01

---
+++ aula01.py
@@ -0,0 +1,13 @@
+import os
+
+#print ("Olá mundo")
+
+print("O 3ds é a turma numero 1 do sagrada")
+turma= "3DS"
+pos=1
+print("o" , turma , "é a turma numero" , pos, "do sagrada")
+
+print(type (turma) )
+print(type (pos) )
+
+os.system("cls")
